5ffd3c9ef3e09e50ebd05ad9db69adfb47304066
akanksha rai <akanksharai@akankshas-MacBook-Air.local>
2025-09-22 13:28:03 +0530

    added ExperienceSection[21637]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import LogoSection from "./sections/LogoSection.jsx"
+import Navbar from "./components/Navbar.jsx"
+import FeatureCards from "./sections/FeatureCards.jsx"
 import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
+import ExperienceSection from "./sections/ExperienceSection.jsx"
 
 const App = () => {
   return (
   <>
+  <Navbar/>
   <Hero/>
   <ShowcaseSection />
+  <LogoSection />
+  <FeatureCards />
+  <ExperienceSection />
   </>
   )
 }
--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    
+
+
+
+    useGSAP(() => {
+        const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+
+        projects.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                {
+                    y: 50,
+                    opacity: 0,
+                },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.3 * (index + 1),
+                    scrollTrigger: {
+                        trigger: card,
+                        start: "top bottom-=100",
+                    }
+                }
+            )
+        })
+
+        gsap.fromTo(
+            sectionRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 1.5 })
+    }, []);
+
     return (
-        <div id='work' className='app-showcase'>
+        <section id='work' ref={sectionRef} className='app-showcase'>
             <div className='w-full'>
                 <div className='showcaselayout'>
                     {/* LEFT */}
-                    <div className='first-project-wrapper'>
+                    <div className='first-project-wrapper' ref={project1Ref}>
                         <div className='image-wrapper'>
                             <img src='/images/project1.png' alt='Ryde' />
                         </div>
                         <div className='text-content'>
-                            <h2>Where Tradtion Meet Luxury Rugs & Carpet E-Commerce 
+                            <h2>Where Tradtion Meet Luxury Rugs & Carpet E-Commerce
                                 called Weavers Rug
                             </h2>
                             <p className='text-white-50 md:text-xl'>
@@ -22,7 +65,7 @@ const ShowcaseSection = () => {
                     </div>
 
                     {/* RIGHT */}
-                    <div className='project-list-wrapper overflow-hidden'>
+                    <div className='project-list-wrapper overflow-hidden' ref={project2Ref}>
                         <div className='project'>
                             <div className='image-wrapper bg-[#ffefdb]'>
                                 <img src='/images/project2.png' alt='Youtube' />
@@ -33,7 +76,7 @@ const ShowcaseSection = () => {
 
                     </div>
 
-                    <div className='project-list-wrapper overflow-hidden'>
+                    <div className='project-list-wrapper overflow-hidden' ref={project3Ref}>
                         <div className='project'>
                             <div className='image-wrapper bg-[#ffe7eb]'>
                                 <img src='/images/project3.png' alt='Youtube' />
@@ -45,7 +88,7 @@ const ShowcaseSection = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
