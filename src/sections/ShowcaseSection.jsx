73fcabb4df2b593361569f6b6f112cd3fa6a1cf2
gauravrai2783 <gauravrai2783@gmail.com>
2025-09-22 10:36:32 +0530

    added project

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -14,10 +14,35 @@ const ShowcaseSection = () => {
                             <h2>Where Tradtion Meet Luxury Rugs & Carpet E-Commerce 
                                 called Weavers Rug
                             </h2>
+                            <p className='text-white-50 md:text-xl'>
+                                a full stackE commerce website functional with payment gateway
+                                and bot chat enable with user friendly experience.
+                            </p>
                         </div>
                     </div>
 
                     {/* RIGHT */}
+                    <div className='project-list-wrapper overflow-hidden'>
+                        <div className='project'>
+                            <div className='image-wrapper bg-[#ffefdb]'>
+                                <img src='/images/project2.png' alt='Youtube' />
+                            </div>
+                            <h2>Full Functional Youtube Clone</h2>
+
+                        </div>
+
+                    </div>
+
+                    <div className='project-list-wrapper overflow-hidden'>
+                        <div className='project'>
+                            <div className='image-wrapper bg-[#ffe7eb]'>
+                                <img src='/images/project3.png' alt='Youtube' />
+                            </div>
+                            <h2>Full Functional Youtube Clone</h2>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
